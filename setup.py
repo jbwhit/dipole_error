889d6529b6d33f5a974425bde1e8d8a674e07e26
Jonathan Whitmore <jbwhit@gmail.com>
2013-01-29 13:49:38 +1100

    restart

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name="dipole_error",
       version="1.0",
       py_modules=['calculation'],
-      scripts=['j222', ],
+      # scripts=['j222', ],
       author='Jonathan Whitmore',
       author_email='jbwhit@gmail.com',
       url='http://jonathanwhitmore.com',
